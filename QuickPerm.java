c13bc7ad41223d9f3efe3adce5f190e760ba7dea
wdong20 <wdong20@stuy.edu>
2021-01-19 19:21:37 -0500

    Created required methods, but haven't implemented them yet

---
+++ QuickPerm.java
@@ -0,0 +1,35 @@
+// Courteous of Phillip Paul Fuchs: https://www.quickperm.org/ Quick Perm (Countdown)
+public class QuickPerm {
+    int[] arr, counter; // 'arr' is the actual array, while counter is used to control the iteration
+    int i;
+    int size;
+
+    // TODO: initialize arr, counter, i, and size
+    public QuickPerm(int length) {
+
+    }
+
+    public int get(int index) {
+        return arr[index];
+    }
+
+    public int size() {
+        return size;
+    }
+
+    // TODO: create end condition
+    public boolean hasNext() {
+        return false;
+    }
+
+    // TODO: implement the actual algorithm
+    public void generateNext() {
+
+    }
+
+    private void swap(int i, int j) {
+        int temp = arr[i];
+        arr[j] = arr[i];
+        arr[i] = temp;
+    }
+}
